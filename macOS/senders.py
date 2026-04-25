8cfd520a9b1fa0eb74ebb5baac9c2e821d3bc59b
Diode-exe <DiodesCAER@protonmail.com>
2026-04-25 14:33:44 -0500

    updated tx_port

--- macOS/senders.py
+++ macOS/senders.py
@@ -3,7 +3,7 @@
 class SenderFunctions:
     """Encapsulates all the functions to send Pelco-D commands based on controller input."""
     def __init__(self):
-        self.tx_port = 'COM6'
+        self.tx_port = 'BG02YH2O'
         self.baud_rate = 9600
         self.address = 1
 
